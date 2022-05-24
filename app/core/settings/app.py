2f47cdd24d9b223e66e661c39dab6b6c3cc59f72
Chiemerie Ezechukwu <50174206+chiemerieezechukwu@users.noreply.github.com>
2022-05-24 09:41:48 +0200

    Define project structure (#1)

---
+++ app/core/settings/app.py
@@ -0,0 +1,54 @@
+import logging
+import sys
+from app.core.settings.base import BaseAppSettings
+from pydantic import PostgresDsn, SecretStr
+
+from loguru import logger
+
+from app.core.logging import InterceptHandler
+
+
+class AppSettings(BaseAppSettings):
+    debug: bool = False
+    docs_url: str = "/docs"
+    openapi_prefix: str = ""
+    openapi_url: str = "/openapi.json"
+    redoc_url: str = "/redoc"
+    title: str = "FastAPI example application"
+    version: str
+
+    database_url: PostgresDsn
+
+    secret_key: SecretStr
+
+    api_prefix: str = "/api"
+
+    token_prefix: str = "Bearer"
+
+    allowed_hosts: list[str]
+
+    logging_level: int = logging.INFO
+    loggers: tuple[str, str] = ("uvicorn.asgi", "uvicorn.access")
+
+    class Config:
+        validate_assignment = True
+
+    @property
+    def fastapi_kwargs(self):
+        return {
+            "debug": self.debug,
+            "docs_url": self.docs_url,
+            "openapi_prefix": self.openapi_prefix,
+            "openapi_url": self.openapi_url,
+            "redoc_url": self.redoc_url,
+            "title": self.title,
+            "version": self.version,
+        }
+
+    def configure_logging(self):
+        logging.getLogger().handlers = [InterceptHandler()]
+        for logger_name in self.loggers:
+            logging_logger = logging.getLogger(logger_name)
+            logging_logger.handlers = [InterceptHandler(level=self.logging_level)]
+
+        logger.configure(handlers=[{"sink": sys.stderr, "level": self.logging_level}])
---
+++ app/core/settings/development.py
@@ -0,0 +1,16 @@
+import logging
+
+from app.core.settings.app import AppSettings
+
+
+class DevAppSettings(AppSettings):
+    debug: bool = True
+    title: str = "Dev FastAPI application"
+    version: str = "Dev version"
+
+    allowed_hosts: list[str] = ["*"]
+
+    logging_level: int = logging.DEBUG
+
+    class Config(AppSettings.Config):
+        env_file = ".env"
