0fa9b7da2007e28b9ad10d5b3321630ca14fbfba
Chiemerie Ezechukwu <50174206+chiemerieezechukwu@users.noreply.github.com>
2022-06-22 14:58:17 +0200

    add auto named constraint naming convention
    ref: https://alembic.sqlalchemy.org/en/latest/naming.html

--- app/db/models/common.py
+++ app/db/models/common.py
@@ -1,11 +1,21 @@
 import uuid
 
-from sqlalchemy import Column, DateTime
+from sqlalchemy import Column, DateTime, MetaData
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import DeclarativeMeta, declarative_base
 from sqlalchemy.sql import func
 
-Base: DeclarativeMeta = declarative_base()
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+metadata = MetaData(naming_convention=convention)
+
+Base: DeclarativeMeta = declarative_base(metadata=metadata)
 
 
 class BaseModelOrm(Base):
